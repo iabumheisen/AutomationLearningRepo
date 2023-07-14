5852f2e519f0ddc3ee0c9e7035102e0085ff7ba3
iabumheisen <iabu@amazon.com>
2023-07-14 14:19:05 +0300

    Implement testNG in maven project

---
+++ src/test/java/com/test/FirstTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+    @Test
+    public void printMethod() {
+
+        System.out.println("Hello Emran");
+    }
+}
